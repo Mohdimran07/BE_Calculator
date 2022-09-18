e50043bf6ea4b83c934253a0ea495a276e7d06d3
Mohammed Imran <mohdimran97057@gmail.com>
2022-09-18 09:58:14 +0530

    first api call

---
+++ app.js
@@ -0,0 +1,8 @@
+const data = [
+    {name:'imran', email:'imran@gmail.com'},
+    {name:'irfan', email:'irfan@gmail.com'},
+    {name:'rehan', email:'rehan@gmail.com'},
+    {name:'subhan', email:'subhan@gmail.com'},
+]
+
+module.exports = data;
